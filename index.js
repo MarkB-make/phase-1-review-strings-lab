1c3efb20e27809cfe644ef4b68aef6330d492db3
Mark <jadebrian999@gmail.com>
2025-10-02 14:21:32 +0300

    changes made

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code in this file!
+
+// 1. Define currentUser variable
+const currentUser = 'Grace Hopper';
+
+// 2. Define welcomeMessage using template literal
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// 3. Define excitedWelcomeMessage using toUpperCase() method
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+// 4. Define shortGreeting using slice() to get first initial
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
